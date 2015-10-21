9688478727c755c87af4cd6774f8a32efe43e303
d.pranchuk <d.pranchuk@itransition.com>
2015-10-21 22:17:31 +0300

    en locale and refactoring

--- main.js
+++ main.js
@@ -10,43 +10,48 @@ Array.prototype.contains = function(obj) {
 
 var rankNames = ["ones", "thousands", "millions", "billions", "trillions"];
 
-var toWords = (number) => {
-    var result = "";
+var toWords = (number, locale) => {
+    var dictionary = i18n[locale].numbers;
+    var result = [];
     var rank = 0;
     while(number > 0) {
-      result = rankToWords(number, rank) + result;
+      result.unshift(rankToWords(number, rank, dictionary));
       rank++;
       number = Math.floor(number/1000);
     }
-    return result;
+    return result.join(" ");
 };
 
-var rankToWords = (number, rank) => {
+var rankToWords = (number, rank, dictionary) => {
     var bufNumber = number % 1000;
-    var bufResult = "";
+    var bufResult = [];
     var plurals = getPlurals(bufNumber);
     var hundredsDigit = Math.floor(bufNumber/100);
     var rankName = rankNames[rank];
     if(hundredsDigit != 0) {
-      bufResult += i18n.ru.numbers.hundreds[hundredsDigit] + " ";
+      bufResult.push(dictionary.hundreds[hundredsDigit]);
     }
     bufNumber = bufNumber % 100;
-    if(bufNumber > 9 && bufNumber < 20) {
-      bufResult += i18n.ru.numbers.teens[bufNumber%10] + " ";
+    if(dictionary.teens && bufNumber > 9 && bufNumber < 20) {
+      bufResult.push(dictionary.teens[bufNumber%10]);
     } else {
       var tenDigit = Math.floor(bufNumber/10);
       if(tenDigit != 0) {
-        bufResult += i18n.ru.numbers.tens[tenDigit] + " ";
+        bufResult.push(dictionary.tens[tenDigit]);
       }
       var oneDigit = Math.floor(bufNumber % 10);
       if(oneDigit != 0) {
-        bufResult += i18n.ru.numbers.ones[sexOfRank[rankName]][oneDigit] + " ";
+        if(dictionary.sexOfRank != undefined) {
+          bufResult.push(dictionary.ones[dictionary.sexOfRank[rankName]][oneDigit]);
+        } else {
+          bufResult.push(dictionary.ones[oneDigit]);
+        }
       }
     }
     if(rankName != "ones") {
-      bufResult += i18n.ru.numbers[rankName][plurals] + " ";
+      bufResult.push(dictionary[rankName][plurals]);
     }
-    return bufResult;
+    return bufResult.join(" ");
 }
 
 var getPlurals = (number) => {
@@ -61,17 +66,16 @@ var getPlurals = (number) => {
     }
 };
 
-var sexOfRank = {
-  ones: "male",
-  thousands: "female",
-  millions: "male",
-  billions: "male",
-  trillions: "male"
-};
-
 var i18n = { ru:
    { numbers:
-      { ones:
+      { sexOfRank: {
+        ones: "male",
+        thousands: "female",
+        millions: "male",
+        billions: "male",
+        trillions: "male"
+        },
+        ones:
          { male:
             [ 'ноль',
               'один',
@@ -171,22 +175,6 @@ var i18n = { ru:
         millions: { one: 'миллион', few: 'миллиона', many: 'миллионов' },
         billions: { one: 'миллиард', few: 'миллиарда', many: 'миллиардов' },
         trillions: { one: 'триллион', few: 'триллионa', many: 'триллионов' },
-        quadrillions:
-         { one: 'квадриллион',
-           few: 'квадриллиона',
-           many: 'квадриллионов' },
-        quintillions:
-         { one: 'квинтиллион',
-           few: 'квинтиллиона',
-           many: 'квинтиллионов' },
-        sextillions:
-         { one: 'секстиллион',
-           few: 'секстиллиона',
-           many: 'секстиллионов' },
-        septillions: { one: 'септиллион', few: 'секстиллиона', many: 'секстиллионов' },
-        octillions: { one: 'октиллион', few: 'октиллиона', many: 'октиллионов' },
-        nonillions: { one: 'нониллион', few: 'нониллиона', many: 'нониллионов' },
-        decillions: { one: 'дециллион', few: 'дециллиона', many: 'дециллионов' },
         mega:
          [ 'ones',
            'thousands',
@@ -199,4 +187,143 @@ var i18n = { ru:
            'septillions',
            'octillions',
            'nonillions',
-           'decillions' ] } } };
+           'decillions' ] } },
+           en:
+     { numbers:
+        { ones:
+           [ 'zero',
+             'one',
+             'two',
+             'three',
+             'four',
+             'five',
+             'six',
+             'seven',
+             'eight',
+             'nine' ],
+          teens:
+           [ 'ten',
+             'eleven',
+             'twelve',
+             'thirteen',
+             'fourteen',
+             'fifteen',
+             'sixteen',
+             'seventeen',
+             'eighteen',
+             'nineteen' ],
+          tens:
+           [ 'zero',
+             'ten',
+             'twenty',
+             'thirty',
+             'forty',
+             'fifty',
+             'sixty',
+             'seventy',
+             'eighty',
+             'ninety' ],
+          hundreds: ['zero',
+                      'one hundred',
+                      'two hundreds',
+                      'three hundreds',
+                      'four hundreds',
+                      'five hundreds',
+                      'six hundreds',
+                      'seven hundreds',
+                      'eight hundreds',
+                      'nine hundreds'],
+          mega:
+           [ 'ones',
+             'thousand',
+             'million',
+             'billion',
+             'trillion',
+             'quadrillion',
+             'quintillion',
+             'sextillion',
+             'septillion',
+             'octillion',
+             'nonillion',
+             'decillion',
+             'undecillion',
+             'duodecillion',
+             'tredecillion',
+             'quattuordecillion' ],
+          ordinal:
+           { ones:
+              [ 'zeroth',
+                'first',
+                'second',
+                'third',
+                'fourth',
+                'fifth',
+                'sixth',
+                'seventh',
+                'eighth',
+                'ninth' ],
+             teens:
+              [ 'tenth',
+                'eleventh',
+                'twelfth',
+                'thirteenth',
+                'fourteenth',
+                'fifteenth',
+                'sixteenth',
+                'seventeenth',
+                'eighteenth',
+                'nineteenth' ],
+             tens:
+              [ 'zeroth',
+                'tenth',
+                'twentieth',
+                'thirtieth',
+                'fortieth',
+                'fiftieth',
+                'sixtieth',
+                'seventieth',
+                'eightieth',
+                'ninetieth' ],
+             mega:
+              [ 'zeroth',
+                'thousandth',
+                'millionth',
+                'billionth',
+                'trillionth',
+                'quadrillionth',
+                'quintillionth',
+                'sextillionth',
+                'septillionth',
+                'octillionth',
+                'nonillionth',
+                'decillionth',
+                'undecillionth',
+                'duodecillionth',
+                'tredecillionth',
+                'quattuordecillionth' ] },
+          union_separator: 'and',
+          micro:
+           [ '_',
+             'tenths',
+             'hundredths',
+             'thousandths',
+             'millionths',
+             'billionths',
+             'trillionths',
+             'quadrillionths',
+             'quintillionths',
+             'sextillionths',
+             'septillionths',
+             'octillionths',
+             'nonillionths',
+             'decillionths',
+             'undecillionths',
+             'duodecillionths',
+             'tredecillionths',
+             'quattuordecillionths' ],
+          tenths: { one: 'tenth', other: 'tenths' },
+          thousands: { one: 'thousand', few:'thousands', many: 'thousands' },
+          millions: { one: 'million', few:'millions', many: 'millions' },
+          billions: { one: 'billion', few: 'billions', many: 'billions' },
+          trillions: { one: 'trillion', few:'trillions', many: 'trillions' },
+          union: 'and' } } };
